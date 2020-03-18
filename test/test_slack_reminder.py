5952278a32f0e5627dccf0a4638db88822387383
shihang <shihang.yu@inceptioniai.org>
2020-03-18 17:42:59 +0400

    [implement] * add slack webhook post api [refactor]
    [bug]
    
    [tmp]

---
+++ test/test_slack_reminder.py
@@ -0,0 +1,3 @@
+'''
+curl -X POST --data-urlencode "payload={\"channel\": \"##bot_reminder\", \"username\": \"webhookbot\", \"text\": \"This is posted to #general and comes from a bot named webhobot.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+'''
